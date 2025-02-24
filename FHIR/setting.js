2bbc82fd76e14b6fe6d1fd91badf0a02f217972d
chiphat102 <112316116@gms.tcu.edu.tw>
2025-02-24 13:08:08 +0800

    Delete FHIR directory

--- FHIR/setting.js
+++
@@ -1,8 +0,0 @@
-var FHIRrootURL ="https://hapi.fhir.org/baseR4/";
-
-// var FHIRrootURL = "https://fhir.tcumi.com:58443/r5/fhir/"
-//var FHIRrootURL = "https://fhir.dicom.tw/fhir/";
-
-// var FHIRrootURL = "https://hapi.fhir.tw/fhir/"
-
- 
